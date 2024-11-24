b29cc7dc06342fc0945727c6d676e86fe942fc7f
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-11-24 17:28:24 -0300

    feat: adds custom task-store request

---
+++ app/Http/Requests/TaskStoreRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TaskStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:100',
+            'description' => 'required|string',
+            'status' => 'required|in:OPEN,IN_PROGRESS,COMPLETED,REJECTED',
+            'building_id' => 'required|integer|exists:buildings,id',
+            'assigned_user_id' => 'required|integer|exists:users,id',
+            'creator_user_id' => 'required|integer|exists:users,id',
+        ];
+    }
+}
