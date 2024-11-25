338a515e1a4e8498853903f94dc7ef514219f487
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-11-25 19:32:06 -0300

    feat: adds TaskIndexRequest

---
+++ app/Http/Requests/TaskIndexRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TaskIndexRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'status' => 'nullable|string|in:OPEN,IN_PROGRESS,COMPLETED,REJECTED',
+            'building_id' => 'nullable|integer|exists:buildings,id',
+            'assigned_user_id' => 'nullable|integer|exists:users,id',
+            'start_date' => 'nullable|date|before_or_equal:end_date',
+            'end_date' => 'nullable|date|after_or_equal:start_date',
+        ];
+    }
+}
