c30f76a2923ce8871843244347119c3f21bcfcf8
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-11-24 22:26:45 -0300

    feat: declares fetch method to TaskRepository contract

--- app/Core/Data/Repositories/TaskRepository.php
+++ app/Core/Data/Repositories/TaskRepository.php
@@ -7,6 +7,14 @@ use App\Core\Domain\Entities\TaskEntity;
 interface TaskRepository
 {
     /**
+     * Fetch tasks along with their comments (based on provided filters)
+     *
+     * @param array<string, string|int> $filters
+     * @return TaskEntity[]
+     */
+    public function fetch(array $filters): array;
+
+    /**
      * Tries to fetch a task that matches the provided id
      *
      * @param int $id
