be8626de45c51df859c587b213889358c55668c7
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-11-25 22:57:27 -0300

    feat: adds index method to TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Core\Domain\UseCases\FetchTasksUseCase;
 use App\Core\Domain\UseCases\StoreTaskUseCase;
+use App\Http\Requests\TaskIndexRequest;
 use App\Http\Requests\TaskStoreRequest;
+use App\Http\Resources\TaskCollection;
 use App\Models\Building;
 use Illuminate\Http\Response;
 
@@ -12,20 +15,37 @@ class TaskController extends Controller
     /**
      * TaskController contructor
      *
+     * @param FetchTasksUseCase $fetchTasksUseCase
      * @param StoreTaskUseCase $storeTaskUseCase
      */
     public function __construct(
+        protected readonly FetchTasksUseCase $fetchTasksUseCase,
         protected readonly StoreTaskUseCase $storeTaskUseCase,
     ) {
     }
 
     /**
-      * Stores a task to the requested building
-      *
-      * @param Building
-      * @param TaskStoreRequest $request
-      * @return Response
-      */
+     * Gets the filtered tasks along with their comments
+     *
+     * @param TaskIndexRequest $request
+     * @return TaskCollection
+     */
+    public function index(TaskIndexRequest $request): TaskCollection
+    {
+        $filters = $request->validated();
+
+        $fetchedTasks = $this->fetchTasksUseCase->fetchTasks($filters);
+
+        return new TaskCollection(collect($fetchedTasks));
+    }
+
+    /**
+     * Stores a task to the requested building
+     *
+     * @param Building
+     * @param TaskStoreRequest $request
+     * @return Response
+     */
     public function store(Building $building, TaskStoreRequest $request): Response
     {
         $payload = $request->safe()->only([
